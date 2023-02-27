cf0af81b6d7cf3a279adb7b3c49652d0324cc955
OgabekMengn1yozov <mengniyozovogabek05@gmail.com>
2023-02-27 09:25:37 +0500

    target facebook

--- app.js
+++ app.js
@@ -15,42 +15,42 @@ mongoose.connect(process.env.MONGO_URL, () => {
 
 const TelegramBot = require("node-telegram-bot-api");
 
-// const bot = new TelegramBot(process.env.TOKEN, {
-//   polling: true,
-// });
+const bot = new TelegramBot(process.env.TOKEN, {
+  polling: true,
+});
 
-// bot.on("message", async (msg) => {
-//   if (msg.from.id == process.env.USER_ID) {
-//     let xl = require("excel4node");
-//     let wb = new xl.Workbook();
-//     const ws = wb.addWorksheet("Sheet 1");
+bot.on("message", async (msg) => {
+  if (msg.from.id == process.env.USER_ID) {
+    let xl = require("excel4node");
+    let wb = new xl.Workbook();
+    const ws = wb.addWorksheet("Sheet 1");
 
-//     ws.cell(1, 1).string("№");
-//     ws.cell(1, 2).string("FISh");
-//     ws.cell(1, 3).string("Telefon");
+    ws.cell(1, 1).string("№");
+    ws.cell(1, 2).string("FISh");
+    ws.cell(1, 3).string("Telefon");
 
-//     const userList = await User.find();
+    const userList = await User.find();
 
-//     for (let i = 0; i < userList.length; i++) {
-//       ws.cell(i + 2, 1).number(i + 1);
-//       ws.cell(i + 2, 2).string(`${userList[i].name}`);
-//       ws.cell(i + 2, 3).string(`${userList[i].phone}`);
-//     }
+    for (let i = 0; i < userList.length; i++) {
+      ws.cell(i + 2, 1).number(i + 1);
+      ws.cell(i + 2, 2).string(`${userList[i].name}`);
+      ws.cell(i + 2, 3).string(`${userList[i].phone}`);
+    }
 
-//     ws.column(1).setWidth(5);
-//     ws.column(2).setWidth(40);
-//     ws.column(3).setWidth(30);
+    ws.column(1).setWidth(5);
+    ws.column(2).setWidth(40);
+    ws.column(3).setWidth(30);
 
-//     wb.write("Excel.xlsx", async function (err, stats) {
-//       if (err) {
-//         bot.sendMessage(process.env.USER_ID, "Xatolik yuz berdi");
-//       } else {
-//         let file_path = path.join(__dirname, "Excel.xlsx");
-//         await bot.sendDocument(process.env.USER_ID, (document = file_path));
-//       }
-//     });
-//   }
-// });
+    wb.write("Excel.xlsx", async function (err, stats) {
+      if (err) {
+        bot.sendMessage(process.env.USER_ID, "Xatolik yuz berdi");
+      } else {
+        let file_path = path.join(__dirname, "Excel.xlsx");
+        await bot.sendDocument(process.env.USER_ID, (document = file_path));
+      }
+    });
+  }
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
